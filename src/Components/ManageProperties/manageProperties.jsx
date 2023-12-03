b99153671c10370edd6ce5517c4b0c84f0870e9d
Tonmoy <shahariartonmoy17@gmail.com>
2023-12-04 00:18:34 +0600

    manage properties design done

--- src/Components/ManageProperties/manageProperties.jsx
+++ src/Components/ManageProperties/manageProperties.jsx
@@ -4,17 +4,17 @@ import { AuthContext } from "../../AuthProvider/authProvider";
 
 const ManageProperties = () => {
     const { user, signOutUser } = useContext(AuthContext);
-    const [bidJobs, setBidJobs] = useState([]);
+    const [properties, setProperties] = useState([]);
 
     
       useEffect(() => {
-        fetch(`https://need-server.vercel.app/properties`)
+        fetch(`http://localhost:3000/properties`)
           .then((res) => res.json())
-          .then((data) => setBidJobs(data));
+          .then((data) => setProperties(data));
       }, []);
 
       const handleUpdateStatusRejected = id =>{
-        fetch(`https://need-server.vercel.app/bidJobs/${id}`, {
+        fetch(`http://localhost:3000/properties/${id}`, {
             method: 'PATCH',
             headers: {
                 'content-type': 'application/json'
@@ -26,11 +26,11 @@ const ManageProperties = () => {
             console.log(data);
             if(data.modifiedCount > 0){
                 //update status
-                const remaining = bidJobs.filter(job => job._id!==id);
-                const updated = bidJobs.find(job => job._id === id);
+                const remaining = properties.filter(job => job._id!==id);
+                const updated = properties.find(job => job._id === id);
                 updated.status = 'rejected';
                 const newBidJobs = [updated, ...remaining];
-                setBidJobs(newBidJobs);
+                setProperties(newBidJobs);
             }
         })
     }
@@ -60,34 +60,34 @@ const ManageProperties = () => {
     return (
         <div className="min-h-screen">
       <h1 className="text-center text-5xl font-bold mb-16 text-[#59CE8F]">
-        My Bids
+        Manage Properties
       </h1>
       <div className="overflow-x-auto container mx-auto">
         <table className="table table-zebra">
           {/* head */}
           <thead>
             <tr>
-              <th className="text-[#59CE8F] text-lg">Job Title</th>
-              <th className="text-[#59CE8F] text-lg">Email</th>
-              <th className="text-[#59CE8F] text-lg">Deadline</th>
-              <th className="text-[#59CE8F] text-lg">Price</th>
-              <th className="text-[#59CE8F] text-lg">Status</th>
+              <th className="text-[#59CE8F] text-lg">Title</th>
+              <th className="text-[#59CE8F] text-lg">Location.</th>
+              <th className="text-[#59CE8F] text-lg">Agent name</th>
+              <th className="text-[#59CE8F] text-lg">Agent email</th>
+              <th className="text-[#59CE8F] text-lg">Price range</th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
             {
-                bidJobs.map(job => (
-                <tr key={job._id} className="border border-[#59CE8F]">
-                    <td>{job.title}</td>
-                    <td>{job.email}</td>
-                    <td>{job.deadline}</td>
-                    <td>{job.price}</td>
-                    <td>{job.status}</td>
-                    <td className=""><button onClick={() => handleUpdateStatusAccepted(job._id)} className={` underline hover:text-[#59CE8F]  mr-4 ${(job.status === 'in progress' || job.status === 'complete' || job.status === 'rejected') ? `hidden` : `block`}`}>Accept</button>
+                properties.map(property => (
+                <tr key={property._id} className="border border-[#59CE8F]">
+                    <td>{property.title}</td>
+                    <td>{property.location}</td>
+                    <td>{property.agentName}</td>
+                    <td>{property.agentEmail}</td>
+                    <td>{property.priceRange}</td>
+                    <td className=""><button onClick={() => handleUpdateStatusAccepted(property._id)} className={` underline hover:text-[#59CE8F]  mr-4 ${(property.status === 'in progress' || property.status === 'complete' || property.status === 'rejected') ? `hidden` : `block`}`}>Verify</button>
 
-                    <button onClick={() => handleUpdateStatusRejected(job._id)} className={`underline hover:text-[#59CE8F] mt-3  ${(job.status === 'in progress' || job.status === 'complete' || job.status === 'rejected') ? `hidden` : `block`}`}>Reject</button>                   
-                    <progress className={`progress w-56 mt-4 ${(job.status === 'in progress') ? `block` : `hidden`}`} value="40" max="100"></progress></td>
+                    <button onClick={() => handleUpdateStatusRejected(property._id)} className={`underline hover:text-[#59CE8F] mt-3  ${(property.status === 'in progress' || property.status === 'complete' || property.status === 'rejected') ? `hidden` : `block`}`}>Reject</button>                   
+                    <progress className={`progress w-56 mt-4 ${(property.status === 'in progress') ? `block` : `hidden`}`} value="40" max="100"></progress></td>
                     
                   </tr>))
             }
